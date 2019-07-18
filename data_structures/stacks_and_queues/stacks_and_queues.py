397c41bb69f76c562bf7d55d8ab706c9ad7e535e
Merry Cimakasky <mkcimakasky@gmail.com>
2019-07-18 15:08:31 -0700

    set up

---
+++ data_structures/stacks_and_queues/stacks_and_queues.py
@@ -0,0 +1,14 @@
+from data_structures.linked_list.linked_list import Node
+
+class Stack: 
+  def __init__(self): 
+    self.top = None 
+
+  def push(self, value): 
+    pass 
+
+  def pop(self): 
+    pass 
+
+  def peek(self): 
+    pass      
