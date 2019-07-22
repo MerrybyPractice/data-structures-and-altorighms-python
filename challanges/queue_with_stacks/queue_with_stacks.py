74c2ca166c899def9bd1db3b5c2225259fa96c29
Merry Cimakasky <mkcimakasky@gmail.com>
2019-07-22 14:08:10 -0700

    start on pseudoqueue

---
+++ challanges/queue_with_stacks/queue_with_stacks.py
@@ -0,0 +1,13 @@
+from data_structures.stacks_and_queues.stacks_and_queues import Stack
+
+class Pseudoqueue: 
+
+  def __init__(self): 
+    self.back = Stack()
+    self.front = Stack() 
+
+  def enqueue(self, value):
+    pass
+
+  def dequeue(self): 
+    pass  
---
+++ challanges/queue_with_stacks/test_queue_with_stacks.py
@@ -0,0 +1,10 @@
+from challanges.queue_with_stacks.queue_with_stacks import Pseudoqueue
+
+def test_pesudoqueue(): 
+  pseudoqueue = Pseudoqueue()
+
+  assert pseudoqueue.front.top == None
+  assert pseudoqueue.back.top == None
+
+def test_enqueue(): 
+  pseudoqueue = Pseudoqueue()
